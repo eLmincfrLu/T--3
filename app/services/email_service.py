ab8f7e8f3c7a6fda0270a3e96ebde91580aef81c
hajieew <hajieew@gmail.com>
2026-08-23 22:06:12 +0400

    salim mail

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -7,7 +7,7 @@ from sib_api_v3_sdk.rest import ApiException
 
 logger = logging.getLogger(__name__)
 
-DEFAULT_TEST_SENDER = "elmincfrlu@gmail.com"
+DEFAULT_TEST_SENDER = "alimhaciyev99@gmail.com"
 
 
 class EmailResult:
